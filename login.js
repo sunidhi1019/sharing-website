3e92ef32052f99243ceda3744998422483a5cb85
Sunidhi Chauhan <59075631+sunidhi1019@users.noreply.github.com>
2020-10-04 17:08:11 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,24 @@
+document.getElementById("clickme").addEventListener('click',
+function(){
+	
+document.querySelector('.bg-modal').style.display ='flex';	
+	
+});
+document.querySelector('.close').addEventListener('click',
+function(){
+document.querySelector('.bg-modal').style.display = 'none';	
+	
+});
+
+//signup
+document.getElementById("clickme2").addEventListener('click',
+function(){
+	
+document.querySelector('.bg-modal2').style.display ='flex';	
+	
+});
+document.querySelector('.close2').addEventListener('click',
+function(){
+document.querySelector('.bg-modal2').style.display = 'none';	
+	
+});
